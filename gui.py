9bb4a10f06dd0405eed68ded887befa4bfa869ca
ghesrob <58829098+ghesrob@users.noreply.github.com>
2020-08-02 21:08:53 +0200

    Performance improvments

--- gui.py
+++ gui.py
@@ -10,12 +10,14 @@ from gui_settings import *
 class DigitGui:
 
     def __init__(self, root, model):
+        # Settings
         self.model = model
         self.root = root
         self.root.title("Digits-CV")
         self.root.iconbitmap("images/main.ico")
         self.root.geometry("600x700")
         self.root.resizable(0,0)
+        # GUI initialization
         self.create_widgets()
         self.ready = True
 
@@ -31,30 +33,30 @@ class DigitGui:
         self.cv.place(x=2, y=35)
         self.cv.bind("<Button-1>", self.activate_painting)
         self.cv.bind("<B1-Motion>", self.paint_lines)
-        # Predict button v2
+        # Predict button
         self.btn_predict2 = tk.Label(self.root, text='Predict',font=myfont, height=2, width=13, bg=color1, fg=white)
         self.btn_predict2.place(relx=0.3, y=630, anchor=tk.CENTER)
+        self.btn_predict2.bind("<Enter>", lambda event: self.btn_predict2.configure(fg=color2))
+        self.btn_predict2.bind("<Leave>", lambda event: self.btn_predict2.configure(fg=white))
         self.btn_predict2.bind("<Button-1>", self.predict)
-        self.btn_predict2.bind("<Enter>", lambda event, h=self.btn_predict2: h.configure(fg=color2))
-        self.btn_predict2.bind("<Leave>", lambda event, h=self.btn_predict2: h.configure(fg=white))
-        # Reset button v2
+        # Reset button
         self.btn_reset2 = tk.Label(self.root, text='Reset',font=myfont, height=2, width=13, bg=color1, fg=white)
+        self.btn_reset2.place(relx=0.7, y=630, anchor=tk.CENTER)
         self.btn_reset2.bind("<Enter>", lambda event: self.btn_reset2.configure(fg=color2))
         self.btn_reset2.bind("<Leave>", lambda event: self.btn_reset2.configure(fg=white))
         self.btn_reset2.bind("<Button-1>", self.reset)
-        self.btn_reset2.place(relx=0.7, y=630, anchor=tk.CENTER)
 
 
     def activate_painting(self, event): 
-        """Helper function for drawing.
-        Triggered when mouse button is pressed over the canvas.
+        """Helper function for painting.
+        Triggered when mouse button is pressed over the drawing surface.
         """
         if self.ready:
             self.x1, self.y1 = event.x, event.y
 
     def paint_lines(self, event):
-        """Draw lines on the canvas.
-        Triggered when the mouse is moved over the canvas, with a button being held down.
+        """Paint lines on the drawing surface.
+        Triggered when the mouse is moved over the drawing surface, with a button being held down.
         """
         if self.ready:
             x2, y2 = event.x, event.y
@@ -76,7 +78,7 @@ class DigitGui:
         self.ready = True
 
     def save_drawing(self, filename):
-        """Cropping of canvas's drawing and saving as a png file"""
+        """Cropping of canvas's paiting and saving as a png file"""
         x_start = root.winfo_rootx() + self.cv.winfo_x()
         y_start = root.winfo_rooty() + self.cv.winfo_y()
         x_end = x_start + self.cv.winfo_width()
@@ -93,31 +95,30 @@ class DigitGui:
         image = cv2.imread(filename, cv2.IMREAD_COLOR)
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-        ret, th = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-        countours = cv2.findContours(th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        _, binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+        countours = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
 
-        color = (80,62,44)
+        for  contour in countours:
 
-        for  cnt in countours:
-            x, y, w, h = cv2.boundingRect(cnt)
-            cv2.rectangle(image, (x,y), (x+w, y+h), color, 1)
-            top = bottom = int(0.05 * th.shape[0])
-            left = right = int(0.05 * th.shape[1])
-            th_up = cv2.copyMakeBorder(th, top, bottom, left, right, cv2.BORDER_REPLICATE)
-            roi = th[y-top:y+h+bottom, x-left:x+w+right]
+            x, y, w, h = cv2.boundingRect(contour)
+            top = bottom = int(0.05 * binary.shape[0])
+            left = right = int(0.05 * binary.shape[1])
+            roi = binary[y-top:y+h+bottom, x-left:x+w+right]
+
+            # Image preprocessing and model prediction
             img = cv2.resize(roi, (28,28), interpolation=cv2.INTER_AREA)
             img = img.reshape(1, 28, 28, 1)
             img = img / 255.0
-
-            pred = self.model.predict([img])[0]
+            pred = self.model.predict(img)[0]
             final_pred = np.argmax(pred)
             
             # Write results on image
             font = cv2.FONT_HERSHEY_SIMPLEX
+            cv2.rectangle(image, (x,y), (x+w, y+h), color_bbox, 1)
             cv2.putText(image, f"Digit: {final_pred}", (x, y-22), fontFace=font, 
-                        fontScale=0.5, color=color, thickness=1, lineType=cv2.LINE_AA)
+                        fontScale=0.5, color=color_bbox, thickness=1, lineType=cv2.LINE_AA)
             cv2.putText(image, f"Confidence: {int(max(pred)*100)}%", (x, y-5), fontFace=font, 
-                        fontScale=0.5, color=color, thickness=1, lineType=cv2.LINE_AA)
+                        fontScale=0.5, color=color_bbox, thickness=1, lineType=cv2.LINE_AA)
 
         # Print result image on screen
         cv2.imwrite(filename, image)
@@ -127,9 +128,8 @@ class DigitGui:
 
 
 
-
 if __name__ == "__main__":
     root = tk.Tk()
-    model = load_model('models/model_15epochs.h5')
+    model = load_model('models/model_gray_scale.h5')
     window = DigitGui(root, model)
     root.mainloop()
---
+++ gui_settings.py
@@ -0,0 +1,9 @@
+"""Some settings to customize GUI's appearance"""
+
+myfont = ("Montserrat", 11)
+
+white = "#ffffff"
+color1 = "#34495e"
+color2 = "#1abc9c"
+color3 = "#2c3e50"
+color_bbox = (80,62,44) #BGR (blue-red-green) format for compatibility with openCV
